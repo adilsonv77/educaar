633e7e021024e41cade2c9814e5f5410d42faa1a
Adilson Vahldick <adilsonv77@gmail.com>
2023-07-13 15:48:33 -0300

    Mais sobre page expired

--- resources/views/layouts/mobile.blade.php
+++ resources/views/layouts/mobile.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="refresh" content="{{ config('session.lifetime') * 60 }}">
     <title>EducaAR</title>
     @yield('style')
     <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon.png">
