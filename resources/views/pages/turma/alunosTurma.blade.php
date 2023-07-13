40f46df4c14c78fb5c852eef21bf21f91546a825
Ana Fábia Coelho <afcoelho44@gmail.com>
2023-07-13 16:07:44 -0300

    ajuste na tela do alunos da turma

--- resources/views/pages/turma/alunosTurma.blade.php
+++ resources/views/pages/turma/alunosTurma.blade.php
@@ -12,7 +12,7 @@
         <div class="card-body">
             @if (!empty($alunos))
                 <div class="table-responsive">
-                    <table class="table align-middle">
+                    <table class="table table-hover table-responsive-sm">
                         <thead>
                             <tr>
                                 <th>Nome</th>
@@ -23,7 +23,36 @@
                             @foreach ($alunos as $item)
                             <tr>
                                     <td>{{ $item->name }}</td>
+                            
+                            <td>
+                                <button type="button"
+                                    class="btn btn-danger"data-toggle="modal" data-target="#modal{{ $item->id }}">
+                                    Desmatricular
+                                </button>
+                            </td>
+                            <div class="modal fade" id="modal{{ $item->id }}" tabindex="-1" role="dialog"
+                                aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-body">
+                                            <h3>Você tem certeza que deseja desmatricular o aluno {{ $item->name }}?
+                                            </h3>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary"
+                                                data-dismiss="modal">Cancelar</button>
+                                            <form action="{{ route('turmas.desmatricular') }}"
+                                                method="POST">
+                                                @csrf
+                                                <input type="hidden" name="aluno_id" value="{{ $item->id }}">
+                                                <button type="submit" class="btn btn-danger">Desmatircular</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
                             </tr>
+                            
+                            </div>
                             @endforeach
 
                         </tbody>
