fadee7033bf647fe1627de5061531d06ff74f01b
LuisKuster <luis.felipe.kuster17@gmail.com>
2023-08-07 17:45:11 -0300

    login_responsivity

--- resources/views/layouts/mobile.blade.php
+++ resources/views/layouts/mobile.blade.php
@@ -14,9 +14,9 @@
         href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Roboto:wght@100;300;400;500;700;900&display=swap"
         rel="stylesheet">
     <link href="/css/style.css" rel="stylesheet">
-    <link rel="stylesheet" href="/css/app.css">
-    <link rel="stylesheet" href="{{ asset('/css/login.css') }}"rel="stylesheet">
-    <link rel="stylesheet" href="/css/questions.css">
+    {{-- <link rel="stylesheet" href="/css/app.css"> --}}
+    {{-- <link rel="stylesheet" href="{{ asset('/css/login.css') }}"rel="stylesheet"> --}}
+    {{-- <link rel="stylesheet" href="/css/questions.css"> --}}
     <link rel="stylesheet" href="/css/mobile.css">
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
--- resources/views/mobHome.blade.php
+++ resources/views/mobHome.blade.php
@@ -5,27 +5,27 @@
         @can('student')
             <div class="card">
                 {{-- <div class="card-body"> --}}
-                <div class="table-responsive">
-                    <table class="table table-hover ">
-                        <tbody>
-                            @foreach ($disciplinas as $item)
-                                <tr>
-                                    <td>
-                                        <form action="{{ route('student.conteudos') }}">
-                                            @csrf
-                                            <input name="id" type="hidden" value="{{ $item->id }}" />
-                                            <button type="submit" class="btn btn-warning">{{ $item->name }}</button>
-                                        </form>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
+                {{-- <div class="table-responsive"> --}}
+                <table class="table table-hover ">
+                    <tbody>
+                        @foreach ($disciplinas as $item)
+                            <tr>
+                                <td>
+                                    <form action="{{ route('student.conteudos') }}">
+                                        @csrf
+                                        <input name="id" type="hidden" value="{{ $item->id }}" />
+                                        <button type="submit" class="btn btn-warning">{{ $item->name }}</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                {{-- </div> --}}
                 {{-- </div> --}}
             </div>
-            @section('script')
-                <script type="module" src="/js/app.js"></script>
+            @section('style')
+                <link href="/css/mobile.css" rel="stylesheet">
             @endsection
         @endcan
     </div>
