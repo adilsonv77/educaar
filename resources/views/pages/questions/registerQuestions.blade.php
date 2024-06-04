e5abc47101a80155be7922294b74d594126138d6
Joao Henrique <joao.hc04@gmail.com>
2024-06-04 16:10:20 -0300

    Modificações cadastro questionário

--- resources/views/pages/questions/registerQuestions.blade.php
+++ resources/views/pages/questions/registerQuestions.blade.php
@@ -7,6 +7,12 @@
         <button class="btn text-white btn-success" wire:click.prevent="addQuestion({{ $id }})">Adicionar
         </button>
     </div> --}}
+    <style>
+        input[type="text"] {
+            width: 650px; 
+        }
+    </style>
+
         <div class="mt-5 mb-5">
             @if ($errors->any())
                 <div class="alert alert-danger">
@@ -32,46 +38,46 @@
             <div class="col-md-9 col-sm-12">
                 <div class="form-group">
                     <label for="">Enunciado</label>
-                    <input type="text" name="question" maxlength="100" class="form-control @error('question') is-invalid @enderror"
+                    <textarea rows="2" cols="75" type="text" name="question" maxlength="150" class="form-control @error('question') is-invalid @enderror"
                         id="question" value="{{ old('question', $question) }}" required autocomplete="question"
-                        autofocus />
+                        autofocus ></textarea>
                 </div>
             </div>
            
     </div>
-    <div class="row mt-3">
+   
         <div class="col-md-3 col-sm-12">
             <div class="form-group">
                 <label for="">Resposta Correta</label>
-                <input type="text" name="A"  maxlength="50" class="form-control @error('A') is-invalid @enderror" id="A"
+                <input  type="text" name="A"  maxlength="75" class="form-control @error('A') is-invalid @enderror" id="A"
                     value="{{ old('A', $A) }}" required autocomplete="A" autofocus />
             </div>
         </div>
         <div class="col-md-3 col-sm-12">
             <div class="form-group">
                 <label for="">Resposta Errada</label>
-                <input type="text" name="B" maxlength="50" class="form-control @error('B') is-invalid @enderror" id="B"
+                <input type="text" name="B" maxlength="75" class="form-control @error('B') is-invalid @enderror" id="B"
                     value="{{ old('B', $B) }}" required autocomplete="B" autofocus />
             </div>
         </div>
         <div class="col-md-3 col-sm-12">
             <div class="form-group">
                 <label for="">Resposta Errada</label>
-                <input type="text" name="C" maxlength="50" class="form-control @error('C') is-invalid @enderror" id="C"
+                <input type="text" name="C" maxlength="75" class="form-control @error('C') is-invalid @enderror" id="C"
                     value="{{ old('C', $C) }}" required autocomplete="C" autofocus />
             </div>
         </div>
         <div class="col-md-3 col-sm-12">
             <div class="form-group">
                 <label for="">Resposta Errada</label>
-                <input type="text" name="D" maxlength="50" class="form-control @error('D') is-invalid @enderror" id="D"
+                <input type="text" name="D" maxlength="75" class="form-control @error('D') is-invalid @enderror" id="D"
                     value="{{ old('D', $D) }}" required autocomplete="D" autofocus />
             </div>
         </div>
         <div class="form-group mt-4 text-center justfy-content-center">
             <input type="submit" value="Salvar" class="btn btn-success">
         </div>
-    </div>
+    
     </form>
     <hr>
     </div>
