409e89fe8101a496e0b9b1e4b8c8c78fad9c782c
Renan G. Klettenberg <121028916+RenanKlettenberg@users.noreply.github.com>
2024-10-15 14:57:30 -0300

    Tela de edição de painel 1.2
    Edição de mídia (imagem/video) feita. Bugs a serem corrigidos. Início da opção de excluir um painel.

--- app/Http/Controllers/PainelController.php
+++ app/Http/Controllers/PainelController.php
@@ -32,16 +32,16 @@ class PainelController extends Controller
 
     public function create()
     {
-        return view('pages.painel.criacaoPaineis', ['titulo'=>'Criação de painéis','action'=>'create']);
+        return view('pages.painel.criacaoPaineis', ['titulo' => 'Criação de painéis', 'action' => 'create']);
     }
 
     public function edit($id)
     {
         $painel = Painei::where('id', $id)->first();
-        if(!empty($painel)) {
+        if (!empty($painel)) {
             $data = json_decode($painel->panel, true);
-            $data['titulo']= 'Edição de painel';
-            $data['action']= 'edit';
+            $data['titulo'] = 'Edição de painel';
+            $data['action'] = 'edit';
             $data['id'] = $id;
 
             return view('pages.painel.criacaoPaineis', $data);
@@ -52,6 +52,7 @@ class PainelController extends Controller
     {
         $data = $request->all();
         $baseFileName = time();
+        $action = $data['action'];
 
         $imgFile = $baseFileName . '.' . $request->arquivoMidia->getClientOriginalExtension();
         $request->arquivoMidia->move(public_path('midiasPainel'), $imgFile);
@@ -59,31 +60,30 @@ class PainelController extends Controller
 
         //Remove os dados desnecessários para serem guardados
         $data['midiaExtension'] = $request->arquivoMidia->getClientOriginalExtension();
-        unset( $data['_token']);
-        unset( $data['midia']);
-        unset( $data['midiasPainel']);
-        $json = ["panel"=>json_encode($data)];
-      
+        unset($data['_token']);
+        unset($data['midia']);
+        unset($data['midiasPainel']);
+        unset($data['action']);
+
+        $json = ["panel" => json_encode($data)];
+
         //Criar o painel e salvo o dado
-        $painel = Painei::create($json);
+        if ($action == 'create') {
+            $painel = Painei::create($json);
+            $id = $painel->id;
+        } else {
+            $id = $data['id'];
+            unset($data['id']);
+            $painel = Painei::where('id', $id)->update($json);
+        }
 
-        $data['midiasPainel'] = $painel->id . '.' . $request->arquivoMidia->getClientOriginalExtension();
+        $data['midiasPainel'] = $id . '.' . $request->arquivoMidia->getClientOriginalExtension();
         $public_path = public_path('midiasPainel');
-
+        if($action == 'edit') unlink($public_path . '/' . $data['midiasPainel']);
         rename($public_path . '/' . $imgFile, $public_path . '/' . $data['midiasPainel']);
-        
-        $painel->update($data);
+
+        // $painel->update($data);
         return redirect()->route('paineis.create');
     }
 
-    public function update(Request $request, $id){
-        $data = $request->all();
-        //Remove os dados desnecessários para serem guardados
-        unset( $data['_token']);
-        unset( $data['midia']);
-        unset( $data['midiasPainel']);
-        $json = ["panel"=>json_encode($data)];
-        Painei::where('id',$id)->update($json);
-        return redirect()->route('paineis.create'); 
-    }
 }
--- resources/views/pages/painel/criacaoPaineis.blade.php
+++ resources/views/pages/painel/criacaoPaineis.blade.php
@@ -8,74 +8,72 @@
 <!-- Bootstrap CSS -->
 <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
 <link href="{{ asset('css/telainicial.css?v=' . filemtime(public_path('css/telainicial.css'))) }}" rel="stylesheet">
-<link rel="stylesheet" href="{{asset('css/painel1.css')}}">
+<link rel="stylesheet" href="{{asset('css/painel.css')}}">
 @endsection
 
 @section('content')
 
 <div class="container-fluid">
-    @if ($action == 'edit')
-        <form action="{{route('paineis.update', ['id'=>$id])}}" method="POST">
-        <input name="id" type="hidden" value = {{$id}}>
-    @else
-        <form action="{{route('paineis.store')}}" method="POST" enctype="multipart/form-data">
-    @endif
-    
-    <input name="action" type="hidden" value = {{$action}}>
-    @csrf
-    @if ($action == 'edit')
-        @method('PUT')
-    @endif
-    <div class="row">
-        <div class="col-md-8 coluna linha">
-            <div id="painel">
-                <textarea name="txtSuperior" id="txtSuperior" type="text" maxlength="117"
-                    placeholder="Digite seu texto aqui"> @if ($action == 'edit') {{$txtSuperior}}
-                    @endif</textarea>
-                <div id="midia" tabindex=0>
-                    <p>Selecione um:</p>
-                    <div id="selectType">
-                        <button id="img">Imagem</button>
-                        <span>ou</span>
-                        <button id="vid">Vídeo</button>
+
+    <form action="{{route('paineis.store')}}" method="POST" enctype="multipart/form-data">
+        @if ($action == 'edit')
+            <input name="id" type="hidden" value={{$id}}>
+        @endif
+        <input name="action" type="hidden" value={{$action}}>
+        @csrf
+        <div class="row">
+            <div class="col-md-8 coluna linha">
+                <div id="painel">
+                    <textarea name="txtSuperior" id="txtSuperior" type="text" maxlength="117"
+                        placeholder="Digite seu texto aqui"> @if ($action == 'edit') {{$txtSuperior}}
+                        @endif</textarea>
+                    <div id="midia" tabindex=0>
+                        <p>Selecione um:</p>
+                        <div id="selectType">
+                            <button id="img">Imagem</button>
+                            <span>ou</span>
+                            <button id="vid">Vídeo</button>
+                        </div>
+                    </div>
+                    <textarea name="txtInferior" id="txtInferior" type="text" maxlength="117"
+                        placeholder="Digite seu texto aqui"> @if ($action == 'edit') {{$txtInferior}}
+                        @endif</textarea>
+                    <div id="areaBtns">
+                        <button id="add">+</button>
                     </div>
                 </div>
-                <textarea name="txtInferior" id="txtInferior" type="text" maxlength="117"
-                    placeholder="Digite seu texto aqui"> @if ($action == 'edit') {{$txtInferior}}
-                    @endif</textarea>
-                <div id="areaBtns">
-                    <button id="add">+</button>
-                </div>
             </div>
-        </div>
-        <div class="col-md-4 coluna">
-            <div id="configPainel">
-                <!--Selecione uma mídia-->
-                <p id="tituloSelecionado">Bloco de Mídia</p>
-                <hr>
-                <div id="blocoMidia">
-                    <p>Tipo de mídia</p>
-                    <input type="radio" name="midia" id="midia1">
-                    <label for="midia1">Video</label>
-                    <input type="radio" name="midia" id="midia2">
-                    <label for="midia2">Imagem</label>
+            <div class="col-md-4 coluna">
+                <div id="configPainel">
+                    <!--Selecione uma mídia-->
+                    <p id="tituloSelecionado">Bloco de Mídia</p>
                     <hr>
-                    <label for="youtubeLink" @if ($action == 'edit') value="{{$link}}" @endif>Youtube</label><br>
-                    <input id="youtubeLink" placeholder="Ex: https://www.youtube.com/watch?v=4YEy" name="link"><br>
-                    <label id="labelMyFile" for="myfile">Local (somente .png, .jpg, .jpeg)</label><br>
-                    <input type="file" style="border:none" name="arquivoMidia" id="midiaInput"
-                        accept=".png, .jpeg, .jpg, .mp4" /><br><br>
-                </div>
-                <div id="blocoTxt">
+                    <div id="blocoMidia">
+                        <p>Tipo de mídia</p>
+                        <input type="radio" name="midia" id="midia1">
+                        <label for="midia1">Video</label>
+                        <input type="radio" name="midia" id="midia2">
+                        <label for="midia2">Imagem</label>
+                        <hr>
+                        <label for="youtubeLink" @if ($action == 'edit') value="{{$link}}" @endif>Youtube</label><br>
+                        <input id="youtubeLink" placeholder="Ex: https://www.youtube.com/watch?v=4YEy" name="link"><br>
+                        <label id="labelMyFile" for="myfile">Local (somente .png, .jpg, .jpeg)</label><br>
+                        <input type="file" style="border:none" name="arquivoMidia" id="midiaInput"
+                            accept=".png, .jpeg, .jpg, .mp4" /><br><br>
+                    </div>
+                    <div id="blocoTxt">
 
-                </div>
-                <div id="blocoBtn">
+                    </div>
+                    <div id="blocoBtn">
 
+                    </div>
+                    @if ($action == 'edit')
+                        <input type="submit" value="Excluir" id="excluirPainel" class="submitPanel">
+                    @endif
+                    <input type="submit" value="Salvar" id="salvarPainel" class="submitPanel">
                 </div>
-                <input type="submit" value="Salvar" id="salvarPainel">
             </div>
         </div>
-    </div>
     </form>
 </div>
 <script src="{{asset('js/painel1.js')}}" type="module"></script>
