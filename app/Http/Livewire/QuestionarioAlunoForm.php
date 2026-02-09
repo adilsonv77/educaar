f7f051ba2bbdef1ca5d5a432e7379da8b722ed29
ThiagoBraz26 <thiagobrazpinheiro@gmail.com>
2026-02-09 16:56:51 -0300

    fix: forget primeira_entrada when save answers

--- app/Http/Livewire/QuestionarioAlunoForm.php
+++ app/Http/Livewire/QuestionarioAlunoForm.php
@@ -286,7 +286,8 @@ class QuestionarioAlunoForm extends Component
             }
             
             DB::commit();
-            session()->forget(['livewire_questoes', 'livewire_alternativas', 'livewire_nrquestao']);
+            session()->forget(['livewire_questoes', 'livewire_alternativas', 'livewire_nrquestao', 'primeira_entrada']);
+            dd(session()->all());
             $this->questions = null;
             if(!$this->incorreta && $content->sort_activities){
                 $this->dispatchBrowserEvent('atividade-concluida', [
