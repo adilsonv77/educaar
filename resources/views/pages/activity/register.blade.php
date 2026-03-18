404f628fefa6a5822f455a117bac17e70fd9d07a
ThiagoBraz26 <thiagobrazpinheiro@gmail.com>
2026-03-18 13:43:14 -0300

    style(activity): fields refeita and pontuada hidden on developer session

--- resources/views/pages/activity/register.blade.php
+++ resources/views/pages/activity/register.blade.php
@@ -144,6 +144,7 @@
                 <div class="form-group">
                     <label for="">Conteúdo*</label>
                     <select class="form-control" name="content_id" aria-label="">
+                        
                         @foreach ($contents as $item)
                             <option value="{{ $item->id }}" data-check="{{ $item->sort }}" 
                             @if ($item->id === ($content ?? $content->first()->id)) selected="selected" @endif>{{ $item->total_name }}</option>
@@ -231,72 +232,73 @@
                 </div>
                 
 
-                <!----------------REFEITA----------------->
-                @if($acao != 'edit')
-                <div class="mb-4">
-                    <div class="custom-control custom-switch switch">
-                        <input type="hidden" name="refeitaMarcador" value="0">
-                        <input type="checkbox" class="custom-control-input" id="refeitaMarcador" name="refeitaMarcador" value="1">
-                        <label class="custom-control-label" for="refeitaMarcador">Refeita</label>
-                        <div class="form-text alert-danger d-inline-block small ml-1 p-0" id="refeitaAlerta" role="alert"><!-- Texto controlado pelo JS --></div>
-                    </div>
-                </div>
-                @endif
-
-                <!----------------PONTUADA----------------->
-                @if($acao != 'edit')
-                    <div class="custom-control custom-switch switch pontuada mb-2 mt-3">
-                        <input type="hidden" name="pontuadaMarcador" value="0">
-                        <input type="checkbox" class="custom-control-input" id="switchPontuada" name="pontuadaMarcador" value="1">
-                        <label class="custom-control-label" for="switchPontuada">Pontuada</label>
-                        <div class="form-text alert-danger d-inline-block small ml-1 p-0" id="pontuadaAlerta" role="alert"><!-- Texto controlado pelo JS --></div>
-
-                        <button type="button" id="btnPontuadaMarcador" data-toggle="modal" data-target="#pontuadaModal" class="btn btn-link p-0 m-0 align-baseline">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-question-lg" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M4.475 5.458c-.284 0-.514-.237-.47-.517C4.28 3.24 5.576 2 7.825 2c2.25 0 3.767 1.36 3.767 3.215 0 1.344-.665 2.288-1.79 2.973-1.1.659-1.414 1.118-1.414 2.01v.03a.5.5 0 0 1-.5.5h-.77a.5.5 0 0 1-.5-.495l-.003-.2c-.043-1.221.477-2.001 1.645-2.712 1.03-.632 1.397-1.135 1.397-2.028 0-.979-.758-1.698-1.926-1.698-1.009 0-1.71.529-1.938 1.402-.066.254-.278.461-.54.461h-.777ZM7.496 14c.622 0 1.095-.474 1.095-1.09 0-.618-.473-1.092-1.095-1.092-.606 0-1.087.474-1.087 1.091S6.89 14 7.496 14"/>
-                            </svg>
-                        </button>
-                    </div>
-                
-                    <!-- Campos extras para caso a atividade seja pontuada (nota e tempo da atividade) -->
-                    <div class="extras collapse" id="extras">
-                        <div class="nota">
-                            <label for="nota">Nota da Atividade</label>
-                            <div class="form-text alert-danger d-inline-block small ml-1 p-0" role="alert">
-                                A nota máxima de uma atividade.
-                            </div>
-                            <input type="number" class="form-control mb-2" name="nota" id="nota" value=100>
-                        </div>
-                        <div class="tempo">
-                            <label for="tempo">Tempo Limite</label>
-                            <div class="form-text alert-danger d-inline-block small ml-1 p-0" role="alert">
-                                Tempo limite para realizar uma atividade completa.
-                            </div>
-                            <input type="number" name="tempo" id="tempo" class="form-control" value=30>
+                @if(!session('type') == 'developer')
+                    <!----------------REFEITA----------------->
+                    @if($acao != 'edit')
+                    <div class="mb-4">
+                        <div class="custom-control custom-switch switch">
+                            <input type="hidden" name="refeitaMarcador" value="0">
+                            <input type="checkbox" class="custom-control-input" id="refeitaMarcador" name="refeitaMarcador" value="1">
+                            <label class="custom-control-label" for="refeitaMarcador">Refeita</label>
+                            <div class="form-text alert-danger d-inline-block small ml-1 p-0" id="refeitaAlerta" role="alert"><!-- Texto controlado pelo JS --></div>
                         </div>
                     </div>
+                    @endif
 
-                    <!-- Modal para explicação da atividade pontuada -->
-                    <div class="modal fade" id="pontuadaModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog modal-lg" role="document">
-                            <div class="modal-content" >
-                                <div class="modal-body">
-                                    <h3>Atividades Pontuadas</h3>
-                                    <p>As atividades pontuadas calculam o desempenho do aluno com base no acerto da resposta e na agilidade, respeitando um tempo limite.</p>
-                                    <p>A pontuação é inversamente proporcional ao tempo gasto: quanto mais demorada a resposta, menor a nota. Respostas incorretas não somam pontos.</p>
-                                    <p>O sistema considera apenas o resultado da primeira tentativa. Mas caso o aluno não tenha acertado todas as questões, poderá refazer a atividade 
-                                    para fins de aprendizado. Nessa nova etapa, não haverá cronômetro e a pontuação original não será alterada.</p>
-                                    <hr>
-                                    <p class="small">Uma atividade pontuada é uma atividade "refeita" com pontuação, logo para uma melhor experiência visual do sistema, restringimos os marcadores para que escolha uma atividade "refeita" ou "pontuada".</p>
+                    <!----------------PONTUADA----------------->
+                    @if($acao != 'edit')
+                        <div class="custom-control custom-switch switch pontuada mb-2 mt-3">
+                            <input type="hidden" name="pontuadaMarcador" value="0">
+                            <input type="checkbox" class="custom-control-input" id="switchPontuada" name="pontuadaMarcador" value="1">
+                            <label class="custom-control-label" for="switchPontuada">Pontuada</label>
+                            <div class="form-text alert-danger d-inline-block small ml-1 p-0" id="pontuadaAlerta" role="alert"><!-- Texto controlado pelo JS --></div>
+
+                            <button type="button" id="btnPontuadaMarcador" data-toggle="modal" data-target="#pontuadaModal" class="btn btn-link p-0 m-0 align-baseline">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-question-lg" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M4.475 5.458c-.284 0-.514-.237-.47-.517C4.28 3.24 5.576 2 7.825 2c2.25 0 3.767 1.36 3.767 3.215 0 1.344-.665 2.288-1.79 2.973-1.1.659-1.414 1.118-1.414 2.01v.03a.5.5 0 0 1-.5.5h-.77a.5.5 0 0 1-.5-.495l-.003-.2c-.043-1.221.477-2.001 1.645-2.712 1.03-.632 1.397-1.135 1.397-2.028 0-.979-.758-1.698-1.926-1.698-1.009 0-1.71.529-1.938 1.402-.066.254-.278.461-.54.461h-.777ZM7.496 14c.622 0 1.095-.474 1.095-1.09 0-.618-.473-1.092-1.095-1.092-.606 0-1.087.474-1.087 1.091S6.89 14 7.496 14"/>
+                                </svg>
+                            </button>
+                        </div>
+
+                        <!-- Campos extras para caso a atividade seja pontuada (nota e tempo da atividade) -->
+                        <div class="extras collapse" id="extras">
+                            <div class="nota">
+                                <label for="nota">Nota da Atividade</label>
+                                <div class="form-text alert-danger d-inline-block small ml-1 p-0" role="alert">
+                                    A nota máxima de uma atividade.
                                 </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                                <input type="number" class="form-control mb-2" name="nota" id="nota" value=100>
+                            </div>
+                            <div class="tempo">
+                                <label for="tempo">Tempo Limite</label>
+                                <div class="form-text alert-danger d-inline-block small ml-1 p-0" role="alert">
+                                    Tempo limite para realizar uma atividade completa.
+                                </div>
+                                <input type="number" name="tempo" id="tempo" class="form-control" value=30>
+                            </div>
+                        </div>
+
+                        <!-- Modal para explicação da atividade pontuada -->
+                        <div class="modal fade" id="pontuadaModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-lg" role="document">
+                                <div class="modal-content" >
+                                    <div class="modal-body">
+                                        <h3>Atividades Pontuadas</h3>
+                                        <p>As atividades pontuadas calculam o desempenho do aluno com base no acerto da resposta e na agilidade, respeitando um tempo limite.</p>
+                                        <p>A pontuação é inversamente proporcional ao tempo gasto: quanto mais demorada a resposta, menor a nota. Respostas incorretas não somam pontos.</p>
+                                        <p>O sistema considera apenas o resultado da primeira tentativa. Mas caso o aluno não tenha acertado todas as questões, poderá refazer a atividade 
+                                        para fins de aprendizado. Nessa nova etapa, não haverá cronômetro e a pontuação original não será alterada.</p>
+                                        <hr>
+                                        <p class="small">Uma atividade pontuada é uma atividade "refeita" com pontuação, logo para uma melhor experiência visual do sistema, restringimos os marcadores para que escolha uma atividade "refeita" ou "pontuada".</p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    @endif
                 @endif
-                    
                 <!-----------------SUBMIT------------------>
                 <div class="form-group mt-4" onsubmit="desativarBotao(this)">
                     <button type="submit" id="btnSalvar" class="btn btn-success">
