755fb2d3b6adc6cdd7ee5494825e62a2973c3d94
José Otávio Ávila Alves Pires <jose.otavio2707@gmail.com>
2026-06-25 16:00:22 -0300

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -123,7 +123,7 @@
                 </div>
                         
                 <div class="data">
-                    20260624
+                    20260625
                 </div>
             </div>   
         </form>
