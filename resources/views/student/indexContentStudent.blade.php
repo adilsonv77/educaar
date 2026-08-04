0fac2857f36fcbb796f93afc342dbb4460ee0185
José Otávio Ávila Alves Pires <jose.otavio2707@gmail.com>
2026-08-04 18:18:34 -0300

    Update indexContentStudent.blade.php

--- resources/views/student/indexContentStudent.blade.php
+++ resources/views/student/indexContentStudent.blade.php
@@ -59,7 +59,7 @@
                                                     <input name="type" type="hidden" value="aluno" />
 
                                                     <button type="submit"
-                                                        class="btn flex-grow-1 {{ $conteudo->is_jogo ? 'btn-warning btn-animado' : ($conteudosRespondidos[$conteudo->id] ? 'btn-secondary text-white' : 'btn-warning') }}">
+                                                        class="btn flex-grow-1 {{ $conteudo->is_jogo ? 'btn-warning btn-animado' : ($conteudosRespondidos[$conteudo->id] ? 'btn-dark btn-warning' : 'btn-warning') }}">
                                                         {{ $conteudo->name }}
                                                     </button>
                                                 </form>
