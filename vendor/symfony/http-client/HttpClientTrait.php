de89373c165bddfe0d802ff1f93e28cfb04bd5ed
ThiagoBraz26 <thiagobrazpinheiro@gmail.com>
2026-08-07 17:31:54 -0300

    chore: add simple-qrcode lib

--- vendor/symfony/http-client/HttpClientTrait.php
+++ vendor/symfony/http-client/HttpClientTrait.php
@@ -13,6 +13,7 @@ namespace Symfony\Component\HttpClient;
 
 use Symfony\Component\HttpClient\Exception\InvalidArgumentException;
 use Symfony\Component\HttpClient\Exception\TransportException;
+use Symfony\Component\HttpClient\Internal\Dechunker;
 use Symfony\Component\HttpClient\Response\StreamableInterface;
 use Symfony\Component\HttpClient\Response\StreamWrapper;
 use Symfony\Component\Mime\MimeTypes;
@@ -170,13 +171,14 @@ trait HttpClientTrait
             unset($options['auth_basic'], $options['auth_bearer']);
 
             // Parse base URI
-            if (\is_string($options['base_uri'])) {
-                $options['base_uri'] = self::parseUrl($options['base_uri']);
+            if (\is_string($baseUri = $options['base_uri'] ?? null)) {
+                $baseUri = self::parseUrl($baseUri);
             }
+            unset($options['base_uri']);
 
             // Validate and resolve URL
             $url = self::parseUrl($url, $options['query']);
-            $url = self::resolveUrl($url, $options['base_uri'], $defaultOptions['query'] ?? []);
+            $url = self::resolveUrl($url, $baseUri, $defaultOptions['query'] ?? []);
         }
 
         // Finalize normalization of options
@@ -355,6 +357,7 @@ trait HttpClientTrait
                     }
                 }
             });
+            $caster = null;
 
             if ('' === $body = http_build_query($body, '', '&')) {
                 return '';
@@ -508,14 +511,15 @@ trait HttpClientTrait
 
     private static function dechunk(string $body): string
     {
-        $h = fopen('php://temp', 'w+');
-        stream_filter_append($h, 'dechunk', \STREAM_FILTER_WRITE);
-        fwrite($h, $body);
-        $body = stream_get_contents($h, -1, 0);
-        rewind($h);
-        ftruncate($h, 0);
+        $dechunker = new Dechunker();
 
-        if (fwrite($h, '-') && '' !== stream_get_contents($h, -1, 0)) {
+        try {
+            $body = $dechunker->dechunk($body);
+        } catch (TransportException) {
+            $dechunker = null;
+        }
+
+        if (!$dechunker?->isFinished()) {
             throw new TransportException('Request body has broken chunked encoding.');
         }
 
@@ -686,11 +690,11 @@ trait HttpClientTrait
 
             if (str_contains($parts[$part], '%')) {
                 // https://tools.ietf.org/html/rfc3986#section-2.3
-                $parts[$part] = preg_replace_callback('/%(?:2[DE]|3[0-9]|[46][1-9A-F]|5F|[57][0-9A]|7E)++/i', fn ($m) => rawurldecode($m[0]), $parts[$part]);
+                $parts[$part] = preg_replace_callback('/%(?:2[DE]|3[0-9]|[46][1-9A-F]|5F|[57][0-9A]|7E)++/i', static fn ($m) => rawurldecode($m[0]), $parts[$part]);
             }
 
             // https://tools.ietf.org/html/rfc3986#section-3.3
-            $parts[$part] = preg_replace_callback("#[^-A-Za-z0-9._~!$&/'()[\]*+,;=:@{}%]++#", fn ($m) => rawurlencode($m[0]), $parts[$part]);
+            $parts[$part] = preg_replace_callback("#[^-A-Za-z0-9._~!$&/'()[\]*+,;=:@{}%]++#", static fn ($m) => rawurlencode($m[0]), $parts[$part]);
         }
 
         return [
